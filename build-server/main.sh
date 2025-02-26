ce40c51e9089cfbecb41729c8f81aa5a77136399
Manish Karki <manishkarki247@gmail.com>
2025-02-26 17:19:16 +0545

    build server script to clone, install, build and upload files to s3

--- build-server/main.sh
+++ build-server/main.sh
@@ -4,4 +4,13 @@ export GIT_REPOSITORY_URL="$GIT_REPOSITORY_URL"
 
 git clone "$GIT_REPOSITORY_URL" /home/app/repo
 
-exec bun script.ts
+cd /home/app/repo
+rm -rf package-lock.json
+cd ../
+#important to include the --include=optional otherwise we get weird gnu linux error
+# https://github.com/npm/cli/issues/4828
+#npm install --legacy-peer-deps --include=optional 
+#npm run build
+
+#ls
+exec node script.js
--- build-server/script.js
+++ build-server/script.js
@@ -1,33 +1,74 @@
-const { exec } = require('chlid_process')
-const path = require('path')
-const fs = require('fs')
+const { exec } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+const dotenv = require("dotenv");
+const mime = require('mime-types')
+
+
+dotenv.config();
+
+const s3Client = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  }
+});
+
+const PROJECT_ID = process.env.PROJECT_ID;
 
 async function init() {
-  const outputPath = path.join(__dirname, "repo")
+  const outputPath = path.join(__dirname, "repo");
 
-  const p = exec(`cd ${outputPath} && bun install && bun run build`)
+  const p = exec(`cd ${outputPath} &&  npm install --legacy-peer-deps --include=optional && npm run build`);
 
   p.stdout.on("data", log => {
-    console.log(log.toString())
-  })
+    console.log("Building app", log);
+  });
 
   p.stdout.on("error", error => {
-    console.log(error.toString())
-  })
+    console.log("Error", error);
+  });
 
-  p.on("close", () => {
-    console.log("Build complete")
-    const distFolderPath = path.join(__dirname, "repo", "dist")
-    const distFolderContents = fs.readdirSync(distFolderPath, { recursive: true })
+  p.on("close", async () => {
+    console.log("Build complete");
+    console.log("envs", {
+      AWS_S3_BUCKET: process.env.AWS_S3_BUCKET,
+      PROJECT_ID: PROJECT_ID,
+      REGION: process.env.AWS_REGION
+    })
+
+    const distFolderPath = path.join(__dirname, "repo", "dist");
+    const distFolderContents = fs.readdirSync(distFolderPath, { recursive: true });
 
     for (const file of distFolderContents) {
-      if (fs.lstatSync(path.join(distFolderPath, file)).isDirectory()) {
-        continue
-      }
+      const filePath = path.join(distFolderPath, file)
+
+      if (fs.lstatSync(filePath).isDirectory()) continue;
+
+
+
+      console.log('uploading', file, mime.lookup(filePath));
+      // publishLog(`uploading ${file}`);
+
+      const command = new PutObjectCommand({
+        Bucket: process.env.AWS_S3_BUCKET,
+        Key: `__outputs/${PROJECT_ID}/${file}`,
+        Body: fs.createReadStream(filePath),
+        ContentType: mime.lookup(filePath)
+      });
+
+      await s3Client.send(command);
+      // publishLog(`uploaded ${file}`);
+      console.log('uploaded', filePath);
 
 
     }
-  })
 
-  console.log("from script.js")
+    process.exit(1)
+  });
+
 }
+
+init();
