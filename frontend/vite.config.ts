3c949f59f108b868818f0d1c4c0374934262b10d
Manish Karki <manishkarki247@gmail.com>
2025-03-08 17:51:45 +0545

    build: expose ports to host for vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,9 +12,11 @@ export default defineConfig({
     },
   },
   server: {
+    host: '0.0.0.0',
     port: process.env.VITE_APP_SERVER_PORT ? Number(process.env.VITE_APP_SERVER_PORT) : 5173,
   },
   preview: {
+    host: '0.0.0.0',
     port: 8999
   }
 })
